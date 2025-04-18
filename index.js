efb6d4b63477f3fa765bdd8a87259dff8f906ccc
Sean Lebida <seanmlebida@cmconnect.cmcc.edu>
2025-04-18 11:18:02 -0400

    Add additional routes for GET and POST methods in the ExpressJS application

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ app.get('/', function(req, res){
 });
 
 app.listen(3007);
+
+var express = require('express');
+var app = express();
+
+app.get('/316', function(req, res){
+   res.send("For God so loved the world, that he gave his only Son, that whoever believes in him should not perish, but have eternal life. John 3:16");
+});
+
+app.post('/hello', function(req, res){
+   res.send("You just called the post method at '/hello'!\n");
+});
+
+app.listen(3008);
