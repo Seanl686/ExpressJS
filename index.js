657f5bfb56453bb637c749213c69edc922633f82
Sean Lebida <seanmlebida@cmconnect.cmcc.edu>
2025-04-18 11:19:35 -0400

    Fix POST route to match existing endpoint for consistency

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.get('/316', function(req, res){
    res.send("For God so loved the world, that he gave his only Son, that whoever believes in him should not perish, but have eternal life. John 3:16");
 });
 
-app.post('/hello', function(req, res){
-   res.send("You just called the post method at '/hello'!\n");
+app.post('/316', function(req, res){
+   res.send("You just called the post method at '/316'!\n");
 });
 
 app.listen(3008);
