d4bfcf8848ba22cf7a4a30ff833be8c9426f9671
Sean Lebida <seanmlebida@cmconnect.cmcc.edu>
2025-04-18 13:03:25 -0400

    Add route for pattern /things/tutorialspoint/id/:id with ID validation

--- index.js
+++ index.js
@@ -49,6 +49,19 @@ app.get('/things/:section/:id', function(req, res) {
    }
 });
 
+// Add route for pattern /things/tutorialspoint/id/12345
+app.get('/things/:section/id/:id', function(req, res) {
+   const section = req.params.section;
+   const id = req.params.id;
+   
+   // Validate the ID format
+   if (/^\d{5}$/.test(id)) {
+      res.send(`Section: ${section}, ID: ${id}`);
+   } else {
+      res.status(400).send('ID must be exactly 5 digits');
+   }
+});
+
 // Add a custom 404 handler - this must be the last route
 app.use(function(req, res, next) {
    res.status(404).send('404 - Sorry, the page you requested was not found.');
